537aa4f1a0ef826f0e31f916dfd9d92e731305f1
John Zhang <haoxian@terpmail.umd.edu>
2021-03-29 15:40:43 -0400

    testing

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
+DATABASE_URL.ssl = { rejectUnauthorized: false };
+
 const db = knex({
   client:'pg',
   connection: DATABASE_URL
