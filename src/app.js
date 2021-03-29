7849252eb42ff1aba72e0618fab25a3920fdada0
John Zhang <haoxian@terpmail.umd.edu>
2021-03-29 18:40:44 -0400

    deploy server

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ app.post('/login', (req, res) =>{
 }
   
 );
-cron.schedule('0 0 * * *', function() {
+cron.schedule('* * * * *', function() {
   console.log('running a task every minute');
   const requestOptions = {
     method: 'GET'
@@ -69,7 +69,7 @@ cron.schedule('0 0 * * *', function() {
   .catch(error => console.log('error', error));
 });
 
-cron.schedule('0 0 * * *', function() {
+cron.schedule('* * * * *', function() {
   console.log('running a task every minute for popular ');
   const requestOptions = {
     method: 'GET'
