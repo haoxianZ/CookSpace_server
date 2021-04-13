69d560309517abf1a05663417ac4359243404787
John Zhang <haoxian@terpmail.umd.edu>
2021-04-13 14:04:18 -0400

    added name fpr event fetch

--- src/events-service.js
+++ src/events-service.js
@@ -6,7 +6,7 @@ const eventsService = {
         return knex.insert(newEvent).into('events').returning('*').then(rows => {return rows[0]})
     },
     getById(knex,id){
-        return knex.select('*').from('events').where('id',id).then(rows => {return rows[0]})
+        return knex('events').join('users','users.id','=','events.host_id').select('events.id','event_recipe_id','event_date','event_name','users.username','host_id').where('events.id',id).then(rows => {return rows[0]})
     },
     getEventsForHost(knex, host_id){
         return knex.select('*').from('events').where('host_id', host_id).whereNot('event_date','=','')
